7b872613b82caab007a579045f7ccdd5f385183d
VenkatKumar S <venkatkumar.s@bambeeq.com>
2013-05-20 18:37:51 +0530

    Added feature collection template

--- js/gooru.js
+++ js/gooru.js
@@ -37,16 +37,13 @@ var helper = {
 	  cache: false,
 	  dataType:'jsonp',
 	  success:function(data){
-	    var html ="";
-	    for(var i in data) {
-	      var imageUrl = data[i].scollections[0].thumbnails.url;
-	      var gooruOid = data[i].scollections[0].gooruOid;
-	       html += '<div class="featuredCollectionBox featuredOne"><a target="_blank" href="http://www.goorulearning.org/gooru/index.g#!/collection/'+gooruOid+'/play"><img src="'+imageUrl+'" width=380 height=240 /></a></div>';
-	    }
-	    
-	      $("#gooruChromeSearchResultContainer").html(html);
+	    EJS.ext=".template";
+	    var featuredCollectionTemplate = new EJS({url:'templates/resource/featured-collection'}).render({data:data});
+	  
+	      $("#gooruChromeSearchResultContainer").html(featuredCollectionTemplate);
 	  }, 
 	error : function(data) {
+	  
 	}
       });    
   }
